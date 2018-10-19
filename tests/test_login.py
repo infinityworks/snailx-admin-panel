d23802854e633a6c31e7efe7bf4c3d314b476c90
Sandeep Sharda <sandeep.sharda@infinityworks.com>
2018-10-19 15:51:10 +0100

    added tests for login page

--- tests/test_login.py
+++ tests/test_login.py
@@ -11,6 +11,18 @@ class TestLogin(unittest.TestCase):
         with self.client as client:
             response = client.get("/login")
             self.assertEqual(200, response.status_code)
+    
+    def test_successful_login(self):
+        with self.client as client:
+            response = self.client.post("/login", data=dict(username="sandeep", password="yes"), follow_redirects=True)
+            self.assertIn(b"Hello World", response.data)
+
+    def test_unsuccessful_login(self):
+        with self.client as client:
+            response = self.client.post("/login", data=dict(username="bob", password="no"), follow_redirects=True)
+            self.assertIn(b"Login Unsuccessful", response.data)
+           
+            
 
 
 if __name__ == '__main__':
