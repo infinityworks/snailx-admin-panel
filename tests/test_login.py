1cf13795dfbff684e83748b831db0d1798a0293d
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-23 14:46:12 +0100

    Remove bcrypt from requirements.txt

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,12 +4,14 @@ from unittest import mock
 from unittest.mock import MagicMock
 from globals.globals import bcrypt
 
+
 class MockUser:
     def __init__(self, id, username, email, password):
         self.id = id
         self.username = username
         self.email = email
         self.password = password
+
 
 class TestLogin(unittest.TestCase):
 
@@ -20,33 +22,37 @@ class TestLogin(unittest.TestCase):
         with self.client as client:
             response = client.get("/login")
             self.assertEqual(200, response.status_code)
-    
+
     @mock.patch("routes.login.login.get_username", MagicMock(return_value=MockUser(1, "Bob", "bob@bob.com", "bob")))
     @mock.patch("routes.login.login.bcrypt.check_password_hash", MagicMock(return_value=True))
     @mock.patch("routes.login.login.login_user", MagicMock(return_value=False))
-    def test_successful_login(self):  
+    def test_successful_login(self):
         with self.client as client:
-            response = self.client.post("/login", data=dict(username="Bob", password="bob"), follow_redirects=True)
-            self.assertIn(b"Hello World", response.data)
+            response = self.client.post(
+                "/login", data=dict(username="Bob", password="bob"), follow_redirects=True)
+            self.assertIn(b"Hello Worldhenretn", response.data)
 
     @mock.patch("routes.login.login.get_username", MagicMock(return_value=MockUser(1, "Bob", "bob@bob.com", "bob")))
     @mock.patch("routes.login.login.bcrypt.check_password_hash", MagicMock(return_value=False))
     def test_unsuccessful_login(self):
         with self.client as client:
-            response = self.client.post("/login", data=dict(username="Jon", password="bob"))
+            response = self.client.post(
+                "/login", data=dict(username="Jon", password="bob"))
             self.assertIn(b"Login Unsuccessful", response.data)
-           
+
     @mock.patch("routes.login.login.get_username", MagicMock(return_value=MockUser(1, "Bob", "bob@bob.com", bcrypt.generate_password_hash("bob").decode("utf-8"))))
-    @mock.patch("routes.login.login.login_user", MagicMock(return_value=False))    
+    @mock.patch("routes.login.login.login_user", MagicMock(return_value=False))
     def test_check_login_with_password(self):
         with self.client as client:
-            response = self.client.post("/login", data=dict(username="Bob", password="bob"), follow_redirects=True)
+            response = self.client.post(
+                "/login", data=dict(username="Bob", password="bob"), follow_redirects=True)
             self.assertIn(b"Hello World", response.data)
 
     @mock.patch("routes.login.login.get_username", MagicMock(return_value=MockUser(1, "Bob", "bob@bob.com", bcrypt.generate_password_hash("fish").decode("utf-8"))))
     def test_check_login_with_incorrect_password(self):
         with self.client as client:
-            response = self.client.post("/login", data=dict(username="Bob", password="bob"), follow_redirects=False)
+            response = self.client.post(
+                "/login", data=dict(username="Bob", password="bob"), follow_redirects=False)
             self.assertIn(b"Login Unsuccessful", response.data)
 
 
