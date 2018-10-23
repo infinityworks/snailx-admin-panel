24196614a67be16475221afc6110c3fc17fff7a0
Malik Glossop <malik.glossop@infinityworks.com>
2018-10-23 09:08:39 +0100

    Added login required for logout route. Added flash message to successful logout.

--- routes/login/logout.py
+++ routes/login/logout.py
@@ -1,14 +1,15 @@
-from flask import render_template, Blueprint, redirect, url_for, flash, request
-from forms.forms import LoginForm
+from flask import render_template, Blueprint, redirect, url_for, flash
 from flask_login import logout_user, login_required
-from globals.globals import login_manager, db
+from globals.globals import login_manager
 from db.models import User
-
 
 
 logout_blueprint = Blueprint('logout', __name__)
 
+
+@login_required
 @logout_blueprint.route("/logout")
 def logout():
     logout_user()
+    flash('Logout successful.')
     return redirect(url_for('login.login'))
