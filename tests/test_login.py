a070b623f4dbf1f545f07d5a5d94fb9c86deaf83
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-23 14:55:10 +0100

    Change login tests.

--- tests/test_login.py
+++ tests/test_login.py
@@ -28,15 +28,15 @@ class TestLogin(unittest.TestCase):
     @mock.patch("routes.login.login.login_user", MagicMock(return_value=False))
     def test_successful_login(self):
         with self.client as client:
-            response = self.client.post(
-                "/login", data=dict(username="Bob", password="bob"), follow_redirects=True)
-            self.assertIn(b"Hello Worldhenretn", response.data)
+            response = client.post(
+                "/login", data=dict(username="Bob", password="bob"))
+            self.assertIn(b"Hello World", response.data)
 
     @mock.patch("routes.login.login.get_username", MagicMock(return_value=MockUser(1, "Bob", "bob@bob.com", "bob")))
     @mock.patch("routes.login.login.bcrypt.check_password_hash", MagicMock(return_value=False))
     def test_unsuccessful_login(self):
         with self.client as client:
-            response = self.client.post(
+            response = client.post(
                 "/login", data=dict(username="Jon", password="bob"))
             self.assertIn(b"Login Unsuccessful", response.data)
 
@@ -44,15 +44,15 @@ class TestLogin(unittest.TestCase):
     @mock.patch("routes.login.login.login_user", MagicMock(return_value=False))
     def test_check_login_with_password(self):
         with self.client as client:
-            response = self.client.post(
-                "/login", data=dict(username="Bob", password="bob"), follow_redirects=True)
+            response = client.post(
+                "/login", data=dict(username="Bob", password="bob"))
             self.assertIn(b"Hello World", response.data)
 
     @mock.patch("routes.login.login.get_username", MagicMock(return_value=MockUser(1, "Bob", "bob@bob.com", bcrypt.generate_password_hash("fish").decode("utf-8"))))
     def test_check_login_with_incorrect_password(self):
         with self.client as client:
-            response = self.client.post(
-                "/login", data=dict(username="Bob", password="bob"), follow_redirects=False)
+            response = client.post(
+                "/login", data=dict(username="Bob", password="bob"))
             self.assertIn(b"Login Unsuccessful", response.data)
 
 
