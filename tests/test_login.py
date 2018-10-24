7d8a119ef7614d2bc84e8fb687654d822ec850d2
Malik Glossop <malik.glossop@infinityworks.com>
2018-10-24 14:57:03 +0100

    Changed login.check_login_with_password to test redirect to rounds page instead of looking for 'Hello World' string in page

--- tests/test_login.py
+++ tests/test_login.py
@@ -42,7 +42,7 @@ class TestLogin(unittest.TestCase):
     @mock.patch("routes.login.login.login_user", MagicMock(return_value=False))    
     def test_check_login_with_password(self):
         with self.client as client:
-            response = self.client.post("/login", data=dict(username="Bob", password="bob"), follow_redirects=True)
+            response = self.client.post("/login", data=dict(username="Bob", password="bob"), follow_redirects=False)
             self.assertEqual(urlparse(response.location).path, url_for('rounds.rounds'))
 
     @mock.patch("routes.login.login.get_username", MagicMock(return_value=MockUser(1, "Bob", "bob@bob.com", bcrypt.generate_password_hash("fish").decode("utf-8"))))
