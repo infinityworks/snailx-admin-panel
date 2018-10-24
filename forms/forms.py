f62a84b6872dba39ebb271b07290f6703e624f08
Michael Silverstone <michael.silverstone@infinityworks.com>
2018-10-24 15:05:14 +0100

    form working

--- forms/forms.py
+++ forms/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
 
@@ -11,6 +11,6 @@ class LoginForm(FlaskForm):
 
 
 class AddRaceForm(FlaskForm):
-    race_date = DateField('Date', validators=[DataRequired()])
+    race_date = StringField('Date', validators=[DataRequired()])
     race_status = StringField('Status', validators=[DataRequired()])
     submit = SubmitField('Add Race')
--- routes/add_race/add_race.py
+++ routes/add_race/add_race.py
@@ -2,21 +2,26 @@ from flask import Blueprint, render_template, url_for, redirect
 from flask_login import current_user
 from forms.forms import AddRaceForm
 from db.models import Race
-
+from globals.globals import db
 
 add_race_blueprint = Blueprint('add_race', __name__)
 
-@add_race_blueprint.route('/rounds/<int:round_id>/add_race', methods=['GET', 'POST'])
+
+@add_race_blueprint.route('/rounds/<int:round_id>/add_race', methods=["GET", "POST"])
 def add_race(round_id):
     if current_user.is_authenticated:  # TODO: NOT
         return redirect(url_for('login.login'))
 
     form = AddRaceForm()
 
-    if form.validate_on_submit():
-        race_date = form.date.data
-        race_status = form.status.data
+    if not form.validate_on_submit():
+        return render_template('add_race.html', form=form)
 
-        return redirect(url_for('rounds.rounds'))
+    race_date = form.race_date.data
+    race_status = form.race_status.data
 
-    return render_template('add_race.html', form=form)
+    race = Race(date=race_date, status=race_status, id_round=round_id)
+    db.session.add(race)
+    db.session.commit()
+
+    return redirect(url_for('rounds.rounds'))
