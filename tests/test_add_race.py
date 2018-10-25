dd7922ed095d218146a11a034dcabcc00df1ef0c
Michael Silverstone <michael.silverstone@infinityworks.com>
2018-10-25 17:05:34 +0100

    tests wip

--- tests/test_add_race.py
+++ tests/test_add_race.py
@@ -8,13 +8,17 @@ class TestAddRace(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
 
-    def test_add_race_response_is_200(self):
+    @mock.patch('flask_login.utils._get_user')
+    def test_add_race_response_is_200(self, username):
         with self.client as client:
+            username.is_authenticated = True
             response = client.get("/rounds/1/races/add")
             self.assertEqual(200, response.status_code)
 
-    @mock.patch("")
-    def test_add_race_form_redirects_to_rounds(self):
+    @mock.patch('flask_login.utils._get_user')
+    def test_add_race_form_redirects_to_rounds(self, username):
         with self.client as client:
-            response = client.post('/rounds/1/race/add', data={"status": "TEST STATUS", "date": "10/25/2018 12:00 AM"})
-            self.assertIn("TEST STATUS", response.data)
+            username.is_authenticated = True
+            response = client.post('/rounds/1/races/add', data=dict(status="TEST STATUS", date="10/25/2018 12:00 AM"),
+                                   follow_redirects=True)
+            self.assertIn("Start Date", str(response.data))
