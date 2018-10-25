c35b0cf2893221ec912379179c3feb378bbfed0c
Malik Glossop <malik.glossop@infinityworks.com>
2018-10-25 16:55:38 +0100

    Hid the Race Participant ID form field and printed the form value

--- forms/forms.py
+++ forms/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms.widgets import HiddenInput
 
 
 class LoginForm(FlaskForm):
@@ -12,6 +13,6 @@ class LoginForm(FlaskForm):
 class RaceResultsForm(FlaskForm):
     time_to_finish = IntegerField('Time to Finish', validators=[DataRequired()])
     position = IntegerField('Position', validators=[DataRequired()])
-    id_race_participants = IntegerField('Participant ID')
+    id_race_participants = IntegerField('Participant ID', widget=HiddenInput())
     did_not_finish = BooleanField('Did Not Finish')
     submit = SubmitField('Submit Result')
