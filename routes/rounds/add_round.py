33cd394aa094dafaf6086a692a1fc8957db2468a
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-26 00:00:53 +0100

    Add validation to round dates.

--- routes/rounds/add_round.py
+++ routes/rounds/add_round.py
@@ -2,6 +2,7 @@ from flask import render_template, Blueprint, redirect, url_for, flash, request
 from forms.forms import AddRoundForm
 from db.models import Round
 from globals.globals import db
+import datetime
 
 add_round_blueprint = Blueprint('add_round', __name__)
 
@@ -14,7 +15,12 @@ def add_round():
         start = form.start_date.data
         end = form.end_date.data
 
-        if not validate_round_dates(start, end):
+        if not validate_dates(start, end):
+            flash(
+                'One or more of the supplied dates are in the past. Please choose dates in the future.', 'danger')
+            return render_template('add_round.html', title='Add Round', form=form)
+
+        if not validate_date_interval(start, end):
             flash(
                 'A round already exists between the given dates. Please select a different start and end date.', 'danger')
             return render_template('add_round.html', title='Add Round', form=form)
@@ -32,5 +38,11 @@ def add_round():
     return render_template('add_round.html', title='Add Round', form=form)
 
 
-def validate_round_dates(start_date, end_date):
+def validate_date_interval(start_date, end_date):
     return not Round().get_num_rounds_between_dates(start_date, end_date)[0]
+
+
+def validate_dates(start_date, end_date):
+    cur_datetime = datetime.datetime.now()
+    return (not datetime.datetime.strptime(start_date, '%m/%d/%Y %H:%M %p') < cur_datetime
+            or not datetime.datetime.strptime(end_date, '%m/%d/%Y %H:%M %p') < cur_datetime)
