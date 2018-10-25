2375abcee9b04eed40d0e557b4ff2e701cdf8de6
Sandeep Sharda <sandeep.sharda@infinityworks.com>
2018-10-25 10:22:44 +0100

    individual race page now shows race participants ids

--- routes/results/results.py
+++ routes/results/results.py
@@ -1,6 +1,6 @@
 from flask import render_template, Blueprint, url_for, redirect
 from flask_login import current_user
-from db.models import RaceResult, Race
+from db.models import RaceResult, Race, RaceParticipants
 
 
 result_blueprint = Blueprint('result', __name__)
@@ -12,5 +12,7 @@ def race(round_id, race_id):
         return redirect(url_for('login.login'))
 
     race = Race().get_race(race_id)
+    participants = RaceParticipants().get_race_participants_race_id(race_id)
 
-    return render_template('index.html', race=race)
+    return render_template('results.html', race=race, participants=participants)
+
