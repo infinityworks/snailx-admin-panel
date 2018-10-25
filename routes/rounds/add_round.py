3ed698bd6e9bcea046f7a1912bd6179bab4d92ad
Matthew Sorsby <matthew.sorsby@infinityworks.com>
2018-10-26 00:12:19 +0100

    Add validation to start and end dates.

--- routes/rounds/add_round.py
+++ routes/rounds/add_round.py
@@ -17,12 +17,17 @@ def add_round():
 
         if not validate_dates(start, end):
             flash(
-                'One or more of the supplied dates are in the past. Please choose dates in the future.', 'danger')
+                'Failed to create new round. One or more of the supplied dates are in the past, please enter valid dates.', 'danger')
+            return render_template('add_round.html', title='Add Round', form=form)
+
+        if not validate_start_before_end(start, end):
+            flash(
+                'Failed to create new round. A round cannot be created with an end date before the start date, please enter valid dates.', 'danger')
             return render_template('add_round.html', title='Add Round', form=form)
 
         if not validate_date_interval(start, end):
             flash(
-                'A round already exists between the given dates. Please select a different start and end date.', 'danger')
+                'Failed to create new round. A round already exists between the given dates, please enter valid dates.', 'danger')
             return render_template('add_round.html', title='Add Round', form=form)
 
         try:
@@ -33,7 +38,7 @@ def add_round():
             return redirect(url_for('rounds.rounds'))
         except:
             flash(
-                'Failed to create new round. Check that round details are valid and try again.', 'danger')
+                'Failed to create new round. Check that round details are valid and try again.', 'error')
 
     return render_template('add_round.html', title='Add Round', form=form)
 
@@ -46,3 +51,7 @@ def validate_dates(start_date, end_date):
     cur_datetime = datetime.datetime.now()
     return (not datetime.datetime.strptime(start_date, '%m/%d/%Y %H:%M %p') < cur_datetime
             or not datetime.datetime.strptime(end_date, '%m/%d/%Y %H:%M %p') < cur_datetime)
+
+
+def validate_start_before_end(start_date, end_date):
+    return datetime.datetime.strptime(start_date, '%m/%d/%Y %H:%M %p') < datetime.datetime.strptime(end_date, '%m/%d/%Y %H:%M %p')
