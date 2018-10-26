7c7a523e5fd04c50c219eabab43cda6d99bd455d
Malik Glossop <malik.glossop@infinityworks.com>
2018-10-26 09:23:39 +0100

    Added constraint to not add race result if a result for that participant exists in the database.

--- routes/results/results.py
+++ routes/results/results.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, url_for, redirect, request
+from flask import render_template, Blueprint, url_for, redirect, request, flash
 from flask_login import current_user
 from db.models import RaceResult, Race, RaceParticipants
 from forms.forms import RaceResultsForm
@@ -16,7 +16,11 @@ def race(round_id, race_id):
     race = Race().get_race(race_id)
     participants = RaceParticipants().get_race_participants_race_id(race_id)
     if race_results_form.submit.data and race_results_form.validate():
-        db.session.add(RaceResult(position=race_results_form.position.data, time_to_finish=race_results_form.time_to_finish.data, did_not_finish=race_results_form.did_not_finish.data, id_race_participants=race_results_form.id_race_participants.data ))
-        db.session.commit()
+        result_row = RaceResult().get_race_result(race_results_form.id_race_participants.data)
+        if not result_row:
+            db.session.add(RaceResult(position=race_results_form.position.data, time_to_finish=race_results_form.time_to_finish.data, did_not_finish=race_results_form.did_not_finish.data, id_race_participants=race_results_form.id_race_participants.data ))
+            db.session.commit()
+        else:
+            flash("The race result has already been submitted for this race participant.")
 
     return render_template('results.html', race=race, participants=participants, race_results_form=race_results_form)
