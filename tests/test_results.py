a4fd70f001e787531a5b0b612ba016a109579e37
Malik Glossop <malik.glossop@infinityworks.com>
2018-10-26 14:44:09 +0100

    Added back validation for race result already in database. Added unittest for results.

--- tests/test_results.py
+++ tests/test_results.py
@@ -24,8 +24,6 @@ class TestResult(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
 
-
-
     @mock.patch('flask_login.utils._get_user')
     def test_results_page_response_is_200(self, current_user):
         with self.client as client:
@@ -41,11 +39,19 @@ class TestResult(unittest.TestCase):
             response = client.get("/rounds/1/races/1")
             self.assertIn(b"Time to Finish", response.data)
 
-
     @mock.patch("db.models.RaceParticipants.get_race_participants_race_id", MagicMock(return_value=[MockRaceParticipants(1,1,1), MockRaceParticipants(2,2,1)]))
     @mock.patch("db.models.Race.get_race", MagicMock(return_value=MockRace(1, datetime.datetime.now(), 'TEST_STATUS', 1)))
     @mock.patch('flask_login.utils._get_user')
-    def test_results_are_displayed(self, current_user):
+    def test_participant_listed(self, current_user):
+        with self.client as client:
+            current_user.is_authenticated = True
+            response = client.get("/rounds/1/races/1")
+            self.assertIn(b"\"id_race_participants\" type=\"hidden\" value=\"2", response.data)
+
+    @mock.patch("db.models.RaceParticipants.get_race_participants_race_id", MagicMock(return_value=[MockRaceParticipants(1,1,1), MockRaceParticipants(3,3,1)]))
+    @mock.patch("db.models.Race.get_race", MagicMock(return_value=MockRace(1, datetime.datetime.now(), 'TEST_STATUS', 1)))
+    @mock.patch('flask_login.utils._get_user')
+    def test_participant_not_listed(self, current_user):
         with self.client as client:
             current_user.is_authenticated = True
             response = client.get("/rounds/1/races/1")
