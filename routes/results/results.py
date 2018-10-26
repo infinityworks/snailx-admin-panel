d14bde135810faeabc820901e88927733576dcb3
Sandeep Sharda <sandeep.sharda@infinityworks.com>
2018-10-26 13:54:40 +0100

    forms now clear on submit, bug fixed where forms would populate with incorrect data

--- routes/results/results.py
+++ routes/results/results.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, url_for, redirect, request, flash
+from flask import render_template, Blueprint, url_for, redirect, request
 from flask_login import current_user
 from db.models import RaceResult, Race, RaceParticipants
 from forms.forms import RaceResultsForm
@@ -16,13 +16,7 @@ def race(round_id, race_id):
     race = Race().get_race(race_id)
     participants = RaceParticipants().get_race_participants_race_id(race_id)
     if race_results_form.submit.data and race_results_form.validate():
-        result_row = RaceResult().get_race_result(race_results_form.id_race_participants.data)
-        if not result_row:
-            flash("Result for Race Participant #{} has been submitted.".format(race_results_form.id_race_participants.data))
-            db.session.add(RaceResult(position=race_results_form.position.data, time_to_finish=race_results_form.time_to_finish.data, did_not_finish=race_results_form.did_not_finish.data, id_race_participants=race_results_form.id_race_participants.data ))
-            db.session.commit()
-            return render_template('results.html', race=race, participants=participants, race_results_form=race_results_form)
-        else:
-            flash("The race result has already been submitted for the race participant with ID {}.".format(result_row.id_race_participants))
-
+        db.session.add(RaceResult(position=race_results_form.position.data, time_to_finish=race_results_form.time_to_finish.data, did_not_finish=race_results_form.did_not_finish.data, id_race_participants=race_results_form.id_race_participants.data ))
+        db.session.commit()
+        return redirect(url_for('result.race', round_id=round_id, race_id=race_id))
     return render_template('results.html', race=race, participants=participants, race_results_form=race_results_form)
