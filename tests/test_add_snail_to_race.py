0be91a98bb6e09ae9975cd739cf7d7e2c80d45a8
Malik Glossop <malik.glossop@infinityworks.com>
2018-10-31 11:29:20 +0000

    Added in validation unittests

--- tests/test_add_snail_to_race.py
+++ tests/test_add_snail_to_race.py
@@ -2,12 +2,17 @@ import unittest
 from globals.globals import app
 from unittest import mock
 from unittest.mock import MagicMock
+import datetime
+from routes.races.add_snail_to_race import (validate_snail_in_same_race,
+                                            flash_redirect,
+                                            validate_snail_in_same_round,
+                                            validate_snail_in_inflight_round)
 
 
 class MockRace:
-    def __init__(self, id, date):
+    def __init__(self, id):
         self.id = id
-        self.date = date
+        self.date = datetime.datetime.now()
         self.status = "TEST PLAYED"
         self.id_round = 1
 
@@ -38,6 +43,32 @@ class TestAddSnailToRace(unittest.TestCase):
 
     def setUp(self):
         self.client = app.test_client()
+
+    @mock.patch('db.models.RaceParticipants.get_race_participants_race_id', MagicMock(return_value=[MockRaceParticipants(1,1,1)]))
+    def test_validate_snail_in_same_race_true(self):
+        self.assertTrue(validate_snail_in_same_race(1, 1))
+
+    @mock.patch('db.models.RaceParticipants.get_race_participants_race_id', MagicMock(return_value=[MockRaceParticipants(1,1,1)]))
+    def test_validate_snail_in_same_race_false(self):
+        self.assertFalse(validate_snail_in_same_race(1, 2))
+
+    @mock.patch('db.models.Race.get_races_by_round', MagicMock(return_value=[MockRace(1)]))
+    @mock.patch('db.models.RaceParticipants.get_race_participants_race_id', MagicMock(return_value=[MockRaceParticipants(1,1,1)]))
+    def test_validate_snail_in_same_round_true(self):
+        self.assertTrue(validate_snail_in_same_round(1, 1, 1))
+
+    @mock.patch('db.models.Race.get_races_by_round', MagicMock(return_value=[MockRace(1)]))
+    @mock.patch('db.models.RaceParticipants.get_race_participants_race_id', MagicMock(return_value=[MockRaceParticipants(1,1,1)]))
+    def test_validate_snail_in_same_round_false(self):
+        self.assertFalse(validate_snail_in_same_round(1, 2, 2))
+
+    @mock.patch("db.models.Round.get_future_round_times",MagicMock(return_value=[MockRound(1, "date")]))
+    def test_validate_snail_in_inflight_round_true(self):
+        self.assertTrue(validate_snail_in_inflight_round(1))
+
+    @mock.patch("db.models.Round.get_future_round_times",MagicMock(return_value=[MockRound(1, "date")]))
+    def test_validate_snail_in_inflight_round_false(self):
+        self.assertFalse(validate_snail_in_inflight_round(2))
 
     @mock.patch('flask_login.utils._get_user')
     def test_snail_to_race_page_response_is_200(self, current_user):
