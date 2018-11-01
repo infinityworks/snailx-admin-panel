e4fbae16af06abc582ec36633032c669e0671b02
Malik Glossop <malik.glossop@infinityworks.com>
2018-11-01 16:25:36 +0000

    Formatted races.py and test_rounds_races.py to pep8 standards

--- tests/test_rounds_races.py
+++ tests/test_rounds_races.py
@@ -35,15 +35,16 @@ class TestRoundsRaces(unittest.TestCase):
             response = client.get("/rounds/1/races")
             self.assertIn(b"TEST_STATUS", response.data)
 
-    @mock.patch("db.models.Race.get_races_by_round", MagicMock(return_value=[]))
+    @mock.patch("db.models.Race.get_races_by_round",
+                MagicMock(return_value=[]))
     @mock.patch('flask_login.utils._get_user')
     def test_no_races_are_displayed(self, current_user):
         with self.client as client:
             current_user.is_authenticated = True
             response = client.get('/round/5/races')
             self.assertNotIn(b"TEST_STATUS", response.data)
- 
-    @mock.patch("routes.races.races.validate_current_round_not_started", 
+
+    @mock.patch("routes.races.races.validate_current_round_not_started",
                 MagicMock(return_value=False))
     @mock.patch("db.models.Race.get_races_by_round", MagicMock(return_value=[
         MockRace(1, datetime.datetime.now(), 'TEST_STATUS', 1),
@@ -56,7 +57,7 @@ class TestRoundsRaces(unittest.TestCase):
             response = self.client.get("/rounds/1/races")
             self.assertIn(b"add-race-disabled", response.data)
 
-    @mock.patch("routes.races.races.validate_current_round_not_started", 
+    @mock.patch("routes.races.races.validate_current_round_not_started",
                 MagicMock(return_value=True))
     @mock.patch("db.models.Race.get_races_by_round", MagicMock(return_value=[
         MockRace(1, datetime.datetime.now(), 'TEST_STATUS', 1),
@@ -69,7 +70,7 @@ class TestRoundsRaces(unittest.TestCase):
             response = self.client.get("/rounds/1/races")
             self.assertIn(b"add-race-enabled", response.data)
 
-    @mock.patch("routes.races.races.validate_current_round_not_started", 
+    @mock.patch("routes.races.races.validate_current_round_not_started",
                 MagicMock(return_value=False))
     @mock.patch("db.models.Race.get_races_by_round", MagicMock(return_value=[
         MockRace(1, datetime.datetime.now(), 'TEST_STATUS', 1),
@@ -82,7 +83,7 @@ class TestRoundsRaces(unittest.TestCase):
             response = self.client.get("/rounds/1/races")
             self.assertIn(b"add-snail-disabled", response.data)
 
-    @mock.patch("routes.races.races.validate_current_round_not_started", 
+    @mock.patch("routes.races.races.validate_current_round_not_started",
                 MagicMock(return_value=True))
     @mock.patch("db.models.Race.get_races_by_round", MagicMock(return_value=[
         MockRace(1, datetime.datetime.now(), 'TEST_STATUS', 1),
@@ -95,9 +96,12 @@ class TestRoundsRaces(unittest.TestCase):
             response = self.client.get("/rounds/1/races")
             self.assertIn(b"add-snail-enabled", response.data)
 
-    @mock.patch("routes.races.races.time_now", MagicMock(return_value=datetime.datetime(2018, 10, 1, 14, 10, 58, 00000)))
+    @mock.patch("routes.races.races.time_now",
+                MagicMock(return_value=datetime.datetime(2018, 10, 1, 14, 10,
+                                                         58, 00000)))
     @mock.patch("db.models.Race.get_races_by_round", MagicMock(return_value=[
-        MockRace(1, datetime.datetime(2018, 11, 1, 14, 10, 58, 00000), 'TEST_STATUS', 1)]))
+        MockRace(1, datetime.datetime(2018, 11, 1, 14, 10, 58, 00000),
+                 'TEST_STATUS', 1)]))
     @mock.patch('flask_login.utils._get_user')
     def test_add_results_button_disabled(self, current_user):
         with self.client as client:
@@ -105,13 +109,15 @@ class TestRoundsRaces(unittest.TestCase):
             response = self.client.get("/rounds/1/races")
             self.assertIn(b"add-results-disabled", response.data)
 
-    @mock.patch("routes.races.races.time_now", MagicMock(return_value=datetime.datetime(2018, 11, 1, 14, 10, 58, 00000)))
+    @mock.patch("routes.races.races.time_now",
+                MagicMock(return_value=datetime.datetime(2018, 11, 1, 14, 10,
+                                                         58, 00000)))
     @mock.patch("db.models.Race.get_races_by_round", MagicMock(return_value=[
-        MockRace(1, datetime.datetime(2018, 10, 1, 14, 10, 58, 00000), 'TEST_STATUS', 1)]))
+        MockRace(1, datetime.datetime(2018, 10, 1, 14, 10, 58, 00000),
+                'TEST_STATUS', 1)]))
     @mock.patch('flask_login.utils._get_user')
     def test_add_results_button_enabled(self, current_user):
         with self.client as client:
             current_user.is_authenticated = True
             response = self.client.get("/rounds/1/races")
             self.assertIn(b"add-results-enabled", response.data)
-            
